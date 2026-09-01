f6c8ca721e4606cfa955d158369674ee11be4894
Nikolaos Gavras <gavras.nikolaos@gmail.com>
2026-09-01 15:27:46 +0300

    did module 5 and 6

--- mod05/06pii.py
+++ mod05/06pii.py
@@ -1,2 +1,14 @@
-import math
+import random
 
+while True:
+    userInput = int(input("Kuinka monta pistettä arvotaan?: "))
+    n = 0
+
+    for _ in range(userInput):
+        x = random.uniform(-1, 1)
+        y = random.uniform(-1, 1)
+        if x**2 + y**2 < 1:
+            n += 1
+
+    pii_likiarvo = 4 * n / userInput
+    print(f"Pii:n likiarvo: {pii_likiarvo}")
